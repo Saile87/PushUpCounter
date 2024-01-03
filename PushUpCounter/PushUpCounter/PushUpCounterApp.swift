112a78fcee13421a6546fc0d89ad883e8dd58493
Elias Breitenbach <elias.b@me.com>
2024-01-03 20:04:34 +0100

    Create a project on github

---
+++ PushUpCounter/PushUpCounter/PushUpCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushUpCounterApp.swift
+//  PushUpCounter
+//
+//  Created by Elias Breitenbach on 03.01.24.
+//
+
+import SwiftUI
+
+@main
+struct PushUpCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
